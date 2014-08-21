be632e1ec23fef6b017865e712f28bdf7e1490a4
Jesse Buss <jessebuss08@gmail.com>
2014-08-21 17:35:17 -0500

    added ColorfulRoad

--- src/Graphs/ColorfulRoad.java
+++ src/Graphs/ColorfulRoad.java
@@ -1,7 +1,59 @@
 package Graphs;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class ColorfulRoad {
-	public int getMin(String road){
-		return 0;
+	public static void main(String[] args){
+		System.out.println(getMin("RGGGB"));
+		System.out.println(getMin("RGBRGBRGB"));
+		System.out.println(getMin("RBBGGGRR"));
+		System.out.println(getMin("RBRRBGGGBBBBR"));
+		System.out.println(getMin("RG"));
+		System.out.println(getMin("RBRGBGBGGBGRGGG"));
+	}
+	
+	public static int getMin(String road){
+		ArrayList<Integer> scores = new ArrayList<Integer>();
+		HashMap<Character,Character> getNext = new HashMap<Character,Character>();
+		getNext.put('R', 'G');
+		getNext.put('G', 'B');
+		getNext.put('B', 'R');
+		
+		getScores(getNext, road, 0, 0, scores);
+		
+		int min = Integer.MAX_VALUE;
+		for (int i : scores){
+			if (i < min)
+				min = i;
+		}
+		if (min == Integer.MAX_VALUE)
+			min = -1;
+		return min;
+	}
+	
+	public static void getScores(HashMap<Character,Character> next, String road, int pos, int score, ArrayList<Integer> scores){
+		//System.out.println("pos = " + pos + ", score = " + score);
+		if (pos == road.length()-1){
+			scores.add(score);
+			return;
+		}
+		char c = next.get(road.charAt(pos));
+		List<Integer> positions = new ArrayList<Integer>();
+		for (int i = pos+1; i < road.length(); i++){
+			if (road.charAt(i) == c)
+			{
+				positions.add(i);
+			}
+		}
+		if (positions.size() == 0)
+		{
+			return;
+		}
+		for (int i : positions){
+			int newScore = score + (i-pos)*(i-pos);
+			getScores(next, road, i, newScore, scores);
+		}
 	}
 }
