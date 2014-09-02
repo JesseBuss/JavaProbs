b46f8688a94b8353ce1c10e9cd045a42880e56fe
JesseBussDesktop <Jesse@10.132.4.111>
2014-09-01 20:28:17 -0500

    PE 14

--- src/ProjectEuler/ProjectEuler14.java
+++ src/ProjectEuler/ProjectEuler14.java
@@ -1,29 +1,25 @@
 package ProjectEuler;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ProjectEuler14 {
 	public static void main(String[] args){
 		int maxLength = 0;
 		int start = 0;
 		for(int i = 13; i < 1000000; i++){
 			long n = i;
-			List<Long> temp = new ArrayList<Long>();
-			temp.add(n);
+			int size = 0;
 			while (n!= 1){
 				if(n%2 == 0)
 					n = n/2;
 				else
 					n = 3*n+1;
-				temp.add(n);
+				size++;
 				
 			}
-			if (temp.size() > maxLength){
-				maxLength = temp.size();
+			if (size > maxLength){
+				maxLength = size;
 				start = i;
 			}
 		}
-		System.out.print("Problem 14 solution = " + start);
+		System.out.print(start);
 	}
 }
